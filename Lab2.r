f1b20af9d55604ba0576f639a2b825ad720b9682
David Valero <david.valero@usach.cl>
2024-11-09 01:32:42 -0300

    Indicaciones de codigo

--- Lab2.r
+++ Lab2.r
@@ -1,1 +1,17 @@
 dataset <- read.csv("wdbc.csv", stringsAsFactors = FALSE)
+
+#Parte 1: Análisis de componentes principales
+
+#Nomralización de datos
+
+#Matriz de covarainza
+
+#Eigenvectors
+
+#Selección de componentes principales
+
+#Parte 2: Clustering 
+
+#K-medoids
+
+
