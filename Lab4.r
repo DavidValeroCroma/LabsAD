09e6264eaaaf3dbc9d371909c781962c07c283f2
Andres Ijurra <andres.ijurra@usach.cl>
2024-12-16 23:20:20 -0300

    Refactor data preprocessing: remove unnecessary columns, check for NA values, and update variable discretization

--- Lab4.r
+++ Lab4.r
@@ -10,9 +10,19 @@ library(caret)
 library(ggplot2)
 
 dataset <- read.csv("wdbc.csv", stringsAsFactors = FALSE)
-dataset$Diagnosis <- as.factor(dataset$Diagnosis)
+
 #Eliminar columna ID porque no aporta al análisis
 dataset$Id <- NULL
+
+## Eliminar columnas con sufijos '2' y '3'
+dataset <- dataset[, !grepl("(2|3)$", names(dataset))]
+dataset$Diagnosis <- as.factor(dataset$Diagnosis)
+## Verificar valores NA
+if (anyNA(dataset)) {
+  print("Existen valores NA en el dataset. Revisa los datos.")
+} else {
+  print("No se encontraron valores NA en el dataset.")
+}
 # Discretización de variables numéricas (por ejemplo, radius1)
 dataset$radius1 <- discretize(dataset$radius1, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
 dataset$perimeter1 <- discretize(dataset$perimeter1, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
@@ -24,26 +34,26 @@ dataset$compactness1 <- discretize(dataset$compactness1, method = "frequency", b
 dataset$concavity1 <- discretize(dataset$concavity1, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
 dataset$symmetry1 <- discretize(dataset$symmetry1, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
 dataset$fractal_dimension1  <- discretize(dataset$fractal_dimension1, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$radius2 <- discretize(dataset$radius2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$perimeter2 <- discretize(dataset$perimeter2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$area2 <- discretize(dataset$area2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$smoothness2 <- discretize(dataset$smoothness2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$concave_points2 <- discretize(dataset$concave_points2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$texture2 <- discretize(dataset$texture2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$compactness2 <- discretize(dataset$compactness2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$concavity2 <- discretize(dataset$concavity2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$symmetry2 <- discretize(dataset$symmetry2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$fractal_dimension2  <- discretize(dataset$fractal_dimension2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$radius3 <- discretize(dataset$radius3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$perimeter3 <- discretize(dataset$perimeter3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$area3 <- discretize(dataset$area3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$smoothness3 <- discretize(dataset$smoothness3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$concave_points3 <- discretize(dataset$concave_points3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$texture3 <- discretize(dataset$texture3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$compactness3 <- discretize(dataset$compactness3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$concavity3 <- discretize(dataset$concavity3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$symmetry3 <- discretize(dataset$symmetry3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
-dataset$fractal_dimension3  <- discretize(dataset$fractal_dimension3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$radius2 <- discretize(dataset$radius2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$perimeter2 <- discretize(dataset$perimeter2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$area2 <- discretize(dataset$area2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$smoothness2 <- discretize(dataset$smoothness2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$concave_points2 <- discretize(dataset$concave_points2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$texture2 <- discretize(dataset$texture2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$compactness2 <- discretize(dataset$compactness2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$concavity2 <- discretize(dataset$concavity2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$symmetry2 <- discretize(dataset$symmetry2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$fractal_dimension2  <- discretize(dataset$fractal_dimension2, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$radius3 <- discretize(dataset$radius3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$perimeter3 <- discretize(dataset$perimeter3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$area3 <- discretize(dataset$area3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$smoothness3 <- discretize(dataset$smoothness3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$concave_points3 <- discretize(dataset$concave_points3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$texture3 <- discretize(dataset$texture3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$compactness3 <- discretize(dataset$compactness3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$concavity3 <- discretize(dataset$concavity3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$symmetry3 <- discretize(dataset$symmetry3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
+# dataset$fractal_dimension3  <- discretize(dataset$fractal_dimension3, method = "frequency", breaks = 3, labels = c("Bajo", "Medio", "Alto"))
 # Dividir el dataset en datos de entrenamiento y prueba
 set.seed(123)
 indices_entrenamiento <- sample(1:nrow(dataset), 0.7 * nrow(dataset))
@@ -62,3 +72,5 @@ predicciones <- predict(modelo, datos_prueba)
 # Evualuar el modelo
 
 confusionMatrix(predicciones, datos_prueba$Diagnosis)
+
+plot(modelo)
